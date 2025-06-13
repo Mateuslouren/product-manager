a32b1f5b7af462d891bd23acc2ad3a4e5739617e
mateus lourenco <mateuslourenco.doc@icloud.com>
2025-06-13 20:21:14 -0300

    Reestrutura projeto: código movido para raiz sem perda

--- product-manager/src/main/java/com/github/mateuslouren/productmanager/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.mateuslouren.productmanager.repository;
-
-import com.github.mateuslouren.productmanager.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
